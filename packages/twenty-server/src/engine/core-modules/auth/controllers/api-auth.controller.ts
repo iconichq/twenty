7285c1c3486867ad9662c7b919f711d4fcb5e0d4
Karl Godard <mitodrummer@gmail.com>
2025-08-12 21:51:33 -0700

    password disabled

--- packages/twenty-server/src/engine/core-modules/auth/controllers/api-auth.controller.ts
+++ packages/twenty-server/src/engine/core-modules/auth/controllers/api-auth.controller.ts
@@ -76,7 +76,7 @@ export class ApiAuthController {
         userWorkspaceId: userWorkspace.id,
         payload: {
           id: workspace.id,
-          // isPasswordAuthEnabled: false,
+          isPasswordAuthEnabled: false,
           defaultRoleId: memberRole?.id,
           displayName: userData.workspaceName,
         },
